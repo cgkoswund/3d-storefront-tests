856ea7030830642dbb2f4d1b19f1962a9262b80a
McTimon <tnyaffram@gmail.com>
2022-09-17 20:39:10 +0000

    Update script.js

--- src/script.js
+++ src/script.js
@@ -1,10 +1,10 @@
-import * as THREE from "three";
-import "./style.css";
-import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
-import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
-import { RGBELoader } from "three/examples/jsm/loaders/RGBELoader.js";
-import GUI from "lil-gui";
-import { DRACOLoader } from "three/examples/jsm/loaders/DRACOLoader.js";
+import * as THREE from 'three';
+import './style.css';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
+import { RGBELoader } from 'three/examples/jsm/loaders/RGBELoader.js';
+import GUI from 'lil-gui';
+import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader.js';
 
 //decimate shoes,
 //fix mouse pointer on hover
@@ -84,7 +84,7 @@ const defaultParams = {
 function init() {
   //   const container = document.createElement("div");
   //   document.body.appendChild(container);
-  canvas = document.querySelector("canvas.webgl");
+  canvas = document.querySelector('canvas.webgl');
 
   camera = new THREE.PerspectiveCamera(
     45,
@@ -96,7 +96,7 @@ function init() {
 
   scene = new THREE.Scene();
 
-  new RGBELoader().setPath("/").load("autoshop.hdr", function (texture) {
+  new RGBELoader().setPath('/').load('autoshop.hdr', function (texture) {
     texture.mapping = THREE.EquirectangularReflectionMapping;
 
     scene.background = texture;
@@ -114,7 +114,7 @@ function init() {
   //floor platform for shoes
   const textureLoader = new THREE.TextureLoader();
   const floorDiffuse = textureLoader.load(
-    "/tex/Wood062_1K-JPG/Wood062_1K_Color.jpg"
+    '/tex/Wood062_1K-JPG/Wood062_1K_Color.jpg'
   );
 
   const floorTable = new THREE.Mesh(
@@ -177,13 +177,13 @@ function init() {
    * LOAD SHOES
    */
   const dracoLoader = new DRACOLoader();
-  dracoLoader.setDecoderPath("/draco/gltf/");
+  dracoLoader.setDecoderPath('/draco/gltf/');
   // asics
   const asics = new THREE.Object3D();
   asicsShoe = asics;
-  const loader = new GLTFLoader().setPath("models/asics_shoe/");
+  const loader = new GLTFLoader().setPath('models/asics_shoe/');
   loader.setDRACOLoader(dracoLoader);
-  loader.load("asics.gltf", function (gltf) {
+  loader.load('asics.gltf', function (gltf) {
     const halfDistance = 0.23;
     const rightLeg = gltf.scene;
     rightLeg.scale.set(4.8, 4.8, 4.8);
@@ -196,8 +196,8 @@ function init() {
     scene.add(asics);
     asics.position.set(1.335, 0.336, -0.225);
     asics.rotation.y = 1.704;
-    const folder = gui.addFolder("Asics");
-    folder.add(defaultParams, "asicsXpos", -2.0, 2.0).onChange((val) => {
+    const folder = gui.addFolder('Asics');
+    folder.add(defaultParams, 'asicsXpos', -2.0, 2.0).onChange((val) => {
       asics.position.x = val;
 
       // render();
@@ -206,12 +206,12 @@ function init() {
     //   asics.position.y = val;
     //   render();
     // });
-    folder.add(defaultParams, "asicsZpos", -2.0, 2.0).onChange((val) => {
+    folder.add(defaultParams, 'asicsZpos', -2.0, 2.0).onChange((val) => {
       asics.position.z = val;
       // render();
     });
 
-    folder.add(defaultParams, "asicsYrot", -4, 4).onChange((val) => {
+    folder.add(defaultParams, 'asicsYrot', -4, 4).onChange((val) => {
       asics.rotation.y = val;
       // render();
     });
@@ -221,8 +221,8 @@ function init() {
   // columbia
   const columbia = new THREE.Object3D();
   columbiaShoe = columbia;
-  loader.setPath("models/columbia_bajada_iii/");
-  loader.load("columbia.gltf", function (gltf) {
+  loader.setPath('models/columbia_bajada_iii/');
+  loader.load('columbia.gltf', function (gltf) {
     const halfDistance = 0.23;
     const rightLeg = gltf.scene;
     rightLeg.scale.set((7 * 0.96) / 1000, (7 * 0.96) / 1000, (7 * 0.96) / 1000);
@@ -238,26 +238,26 @@ function init() {
     columbia.rotation.y = -2.89;
     scene.add(columbia);
 
-    const folder = gui.addFolder("Columbia");
-    folder.add(defaultParams, "columbiaXpos", -2.0, 2.0).onChange((val) => {
+    const folder = gui.addFolder('Columbia');
+    folder.add(defaultParams, 'columbiaXpos', -2.0, 2.0).onChange((val) => {
       columbia.position.x = val;
 
       // render();
     });
-    folder.add(defaultParams, "columbiaYpos", -2.0, 2.0).onChange((val) => {
+    folder.add(defaultParams, 'columbiaYpos', -2.0, 2.0).onChange((val) => {
       columbia.position.y = val;
       // render();
     });
-    folder.add(defaultParams, "columbiaZpos", -2.0, 2.0).onChange((val) => {
+    folder.add(defaultParams, 'columbiaZpos', -2.0, 2.0).onChange((val) => {
       columbia.position.z = val;
       // render();
     });
 
-    folder.add(defaultParams, "columbiaYrot", -4, 4).onChange((val) => {
+    folder.add(defaultParams, 'columbiaYrot', -4, 4).onChange((val) => {
       columbia.rotation.y = val;
       // render();
     });
-    folder.add(defaultParams, "columbiaScale", 0, 1).onChange((val) => {
+    folder.add(defaultParams, 'columbiaScale', 0, 1).onChange((val) => {
       columbia.scale.setScalar(val);
       // render();
     });
@@ -269,8 +269,8 @@ function init() {
   // gucci
   const gucci = new THREE.Object3D();
   gucciShoe = gucci;
-  loader.setPath("models/gucci_shoes/");
-  loader.load("gucci.gltf", function (gltf) {
+  loader.setPath('models/gucci_shoes/');
+  loader.load('gucci.gltf', function (gltf) {
     const halfDistance = -0.09;
     const rightLeg = gltf.scene;
     rightLeg.scale.set(
@@ -294,8 +294,8 @@ function init() {
     gucci.rotation.y = -1.912;
     scene.add(gucci);
 
-    const folder = gui.addFolder("Gucci");
-    folder.add(defaultParams, "gucciXpos", -2.0, 2.0).onChange((val) => {
+    const folder = gui.addFolder('Gucci');
+    folder.add(defaultParams, 'gucciXpos', -2.0, 2.0).onChange((val) => {
       gucci.position.x = val;
       // render();
     });
@@ -303,17 +303,17 @@ function init() {
     //   gucci.position.y = val;
     //   render();
     // });
-    folder.add(defaultParams, "gucciZpos", -2.0, 2.0).onChange((val) => {
+    folder.add(defaultParams, 'gucciZpos', -2.0, 2.0).onChange((val) => {
       gucci.position.z = val;
       // render();
     });
 
-    folder.add(defaultParams, "gucciYrot", -4, 4).onChange((val) => {
+    folder.add(defaultParams, 'gucciYrot', -4, 4).onChange((val) => {
       gucci.rotation.y = val;
       // render();
     });
 
-    folder.add(defaultParams, "gucciScale", 0, 1).onChange((val) => {
+    folder.add(defaultParams, 'gucciScale', 0, 1).onChange((val) => {
       gucci.scale.setScalar(val);
       // render();
     });
@@ -325,8 +325,8 @@ function init() {
   // newBalance
   const newBalance = new THREE.Object3D();
   newBalanceShoe = newBalance;
-  loader.setPath("models/new-balance-fresh-foam-roav/");
-  loader.load("newBalance.gltf", function (gltf) {
+  loader.setPath('models/new-balance-fresh-foam-roav/');
+  loader.load('newBalance.gltf', function (gltf) {
     const halfDistance = 0.23;
     const rightLeg = gltf.scene;
     rightLeg.scale.set(4.6 * 0.85, 4.6 * 0.85, 4.6 * 0.85);
@@ -340,25 +340,25 @@ function init() {
     newBalance.rotation.y = -0.816;
     scene.add(newBalance);
 
-    const folder = gui.addFolder("New Balance");
-    folder.add(defaultParams, "newBalanceXpos", -2.0, 2.0).onChange((val) => {
+    const folder = gui.addFolder('New Balance');
+    folder.add(defaultParams, 'newBalanceXpos', -2.0, 2.0).onChange((val) => {
       newBalance.position.x = val;
       // render();
     });
-    folder.add(defaultParams, "newBalanceYpos", -2.0, 2.0).onChange((val) => {
+    folder.add(defaultParams, 'newBalanceYpos', -2.0, 2.0).onChange((val) => {
       newBalance.position.y = val;
       // render();
     });
-    folder.add(defaultParams, "newBalanceZpos", -2.0, 2.0).onChange((val) => {
+    folder.add(defaultParams, 'newBalanceZpos', -2.0, 2.0).onChange((val) => {
       newBalance.position.z = val;
       // render();
     });
 
-    folder.add(defaultParams, "newBalanceYrot", -4, 4).onChange((val) => {
+    folder.add(defaultParams, 'newBalanceYrot', -4, 4).onChange((val) => {
       newBalance.rotation.y = val;
       // render();
     });
-    folder.add(defaultParams, "newBalanceScale", 0, 1).onChange((val) => {
+    folder.add(defaultParams, 'newBalanceScale', 0, 1).onChange((val) => {
       newBalance.scale.setScalar(val);
       // render();
     });
@@ -369,8 +369,8 @@ function init() {
   // nikeZoom
   const nikeZoom = new THREE.Object3D();
   nikeZoomShoe = nikeZoom;
-  loader.setPath("models/nike_zoom_pegasus_shoe_by_digitworlds.com_-/");
-  loader.load("scene.gltf", function (gltf) {
+  loader.setPath('models/nike_zoom_pegasus_shoe_by_digitworlds.com_-/');
+  loader.load('scene.gltf', function (gltf) {
     const halfDistance = 1.67;
     const rightLeg = gltf.scene; //.children[0].children[0];
     rightLeg.scale.set(12 / 100, 12 / 100, 12 / 100);
@@ -386,8 +386,8 @@ function init() {
     nikeZoom.rotation.y = 0;
     scene.add(nikeZoom);
 
-    const folder = gui.addFolder("Nike Zoom");
-    folder.add(defaultParams, "nikeZoomXpos", -2.0, 2.0).onChange((val) => {
+    const folder = gui.addFolder('Nike Zoom');
+    folder.add(defaultParams, 'nikeZoomXpos', -2.0, 2.0).onChange((val) => {
       nikeZoom.position.x = val;
       // render();
     });
@@ -395,12 +395,12 @@ function init() {
     //   nikeZoom.position.y = val;
     //   render();
     // });
-    folder.add(defaultParams, "nikeZoomZpos", -2.0, 2.0).onChange((val) => {
+    folder.add(defaultParams, 'nikeZoomZpos', -2.0, 2.0).onChange((val) => {
       nikeZoom.position.z = val;
       // render();
     });
 
-    folder.add(defaultParams, "nikeZoomYrot", -4, 4).onChange((val) => {
+    folder.add(defaultParams, 'nikeZoomYrot', -4, 4).onChange((val) => {
       nikeZoom.rotation.y = val;
       // render();
     });
@@ -411,8 +411,8 @@ function init() {
   // sieviRacer
   const sieviRacer = new THREE.Object3D();
   sieviRacerShoe = sieviRacer;
-  loader.setPath("models/sievi_racer_safety_shoe/");
-  loader.load("sieviRacer.gltf", function (gltf) {
+  loader.setPath('models/sievi_racer_safety_shoe/');
+  loader.load('sieviRacer.gltf', function (gltf) {
     const halfDistance = 0.23;
     const rightLeg = gltf.scene;
     rightLeg.scale.set(18 / 100, 18 / 100, 18 / 100);
@@ -426,8 +426,8 @@ function init() {
     sieviRacer.rotation.y = 2.688;
     scene.add(sieviRacer);
 
-    const folder = gui.addFolder("Sievi Racer");
-    folder.add(defaultParams, "sieviXpos", -2.0, 2.0).onChange((val) => {
+    const folder = gui.addFolder('Sievi Racer');
+    folder.add(defaultParams, 'sieviXpos', -2.0, 2.0).onChange((val) => {
       sieviRacer.position.x = val;
       // render();
     });
@@ -435,12 +435,12 @@ function init() {
     //   sieviRacer.position.y = val;
     //   render();
     // });
-    folder.add(defaultParams, "sieviZpos", -2.0, 2.0).onChange((val) => {
+    folder.add(defaultParams, 'sieviZpos', -2.0, 2.0).onChange((val) => {
       sieviRacer.position.z = val;
       // render();
     });
 
-    folder.add(defaultParams, "sieviYrot", -4, 4).onChange((val) => {
+    folder.add(defaultParams, 'sieviYrot', -4, 4).onChange((val) => {
       sieviRacer.rotation.y = val;
       // render();
     });
@@ -451,8 +451,8 @@ function init() {
   // Vans
   const vans = new THREE.Object3D();
   vansShoe = vans;
-  loader.setPath("models/vans_shoe/");
-  loader.load("scene.gltf", function (gltf) {
+  loader.setPath('models/vans_shoe/');
+  loader.load('scene.gltf', function (gltf) {
     const halfDistance = 2.86;
     const rightLeg = gltf.scene;
     rightLeg.scale.set(5 / 1000, 5 / 1000, 5 / 1000);
@@ -468,8 +468,8 @@ function init() {
     vans.rotation.y = 1.048;
     scene.add(vans);
 
-    const folder = gui.addFolder("Vans");
-    folder.add(defaultParams, "vansXpos", -2.0, 2.0).onChange((val) => {
+    const folder = gui.addFolder('Vans');
+    folder.add(defaultParams, 'vansXpos', -2.0, 2.0).onChange((val) => {
       vans.position.x = val;
       // render();
     });
@@ -477,12 +477,12 @@ function init() {
     //   vans.position.y = val;
     //   render();
     // });
-    folder.add(defaultParams, "vansZpos", -2.0, 2.0).onChange((val) => {
+    folder.add(defaultParams, 'vansZpos', -2.0, 2.0).onChange((val) => {
       vans.position.z = val;
       // render();
     });
 
-    folder.add(defaultParams, "vansYrot", -4, 4).onChange((val) => {
+    folder.add(defaultParams, 'vansYrot', -4, 4).onChange((val) => {
       vans.rotation.y = val;
       // render();
     });
@@ -509,7 +509,7 @@ function init() {
   controls.target.set(0, 0, -0.2);
   controls.update();
 
-  window.addEventListener("resize", onWindowResize);
+  window.addEventListener('resize', onWindowResize);
 }
 
 function onWindowResize() {
@@ -518,7 +518,7 @@ function onWindowResize() {
 
   renderer.setSize(window.innerWidth, window.innerHeight);
   // gui.update();
-  // render();
+  /// render();
 }
 
 //
@@ -527,7 +527,7 @@ function render() {
   renderer.render(scene, camera);
 }
 let isIntersecting = false;
-let clickedShoe = "";
+let clickedShoe = '';
 function tick() {
   window.requestAnimationFrame(tick);
   render();
@@ -539,14 +539,14 @@ function tick() {
     const asicsTest = raycaster.intersectObject(asicsShoe, true);
     if (asicsTest.length > 0) {
       isIntersecting = true;
-      clickedShoe = "asics";
+      clickedShoe = 'asics';
     }
   }
   if (columbiaShoe) {
     const columbiaTest = raycaster.intersectObject(columbiaShoe, true);
     if (columbiaTest.length > 0) {
       isIntersecting = true;
-      clickedShoe = "columbia";
+      clickedShoe = 'columbia';
     }
   }
   if (gucciShoe) {
@@ -554,41 +554,41 @@ function tick() {
     if (gucciTest.length > 0)
       if (gucciTest[0].point.y > 0.05) {
         isIntersecting = true;
-        clickedShoe = "gucci";
+        clickedShoe = 'gucci';
       }
   }
   if (newBalanceShoe) {
     const newBalanceTest = raycaster.intersectObject(newBalanceShoe, true);
     if (newBalanceTest.length > 0) {
       isIntersecting = true;
-      clickedShoe = "newBalance";
+      clickedShoe = 'newBalance';
     }
   }
   if (nikeZoomShoe) {
     const nikeZoomTest = raycaster.intersectObject(nikeZoomShoe, true);
     if (nikeZoomTest.length > 0) {
       isIntersecting = true;
-      clickedShoe = "nikeZoom";
+      clickedShoe = 'nikeZoom';
     }
   }
   if (sieviRacerShoe) {
     const sieviRacerTest = raycaster.intersectObject(sieviRacerShoe, true);
     if (sieviRacerTest.length > 0) {
       isIntersecting = true;
-      clickedShoe = "sieviRacer";
+      clickedShoe = 'sieviRacer';
     }
   }
   if (vansShoe) {
     const vansTest = raycaster.intersectObject(vansShoe, true);
     if (vansTest.length > 0) {
       isIntersecting = true;
-      clickedShoe = "vans";
+      clickedShoe = 'vans';
     }
   }
-  if (isIntersecting) canvas.style.cursor = "pointer";
+  if (isIntersecting) canvas.style.cursor = 'pointer';
   else {
-    canvas.style.cursor = "auto";
-    clickedShoe = "";
+    canvas.style.cursor = 'auto';
+    clickedShoe = '';
   }
 }
 
@@ -598,69 +598,69 @@ let isMouseDOwn = false,
   isDragging = false,
   isMoving = false;
 
-window.addEventListener("pointerdown", () => {
+window.addEventListener('pointerdown', () => {
   isMouseDOwn = true;
 });
-window.addEventListener("pointermove", (e) => {
+window.addEventListener('pointermove', (e) => {
   cursor.x = 2 * (e.clientX / window.innerWidth) - 1;
   cursor.y = -2 * (e.clientY / window.innerHeight) + 1;
 
   if (isMouseDOwn) isDragging = true;
 });
 
-window.addEventListener("pointerup", () => {
+window.addEventListener('pointerup', () => {
   if (!isDragging) {
     //then perform click
 
     switch (clickedShoe) {
-      case "":
+      case '':
         break;
-      case "asics":
+      case 'asics':
         window.open(
-          "https://www.amazon.com/ASICS-Womens-Gel-Venture-Running-Black/dp/B091KGFSRL/ref=sr_1_1?crid=76EV940O3ERW&keywords=asics%2Bgel%2Bventure%2B8%2Bwomen&qid=1659010379&sprefix=asics%2Bgel%2Bventure%2B8%2Caps%2C784&sr=8-1&th=1",
-          "_blank"
+          'https://www.amazon.com/ASICS-Womens-Gel-Venture-Running-Black/dp/B091KGFSRL/ref=sr_1_1?crid=76EV940O3ERW&keywords=asics%2Bgel%2Bventure%2B8%2Bwomen&qid=1659010379&sprefix=asics%2Bgel%2Bventure%2B8%2Caps%2C784&sr=8-1&th=1',
+          '_blank'
         );
         break;
 
-      case "columbia":
+      case 'columbia':
         window.open(
-          "https://www.amazon.com/Columbia-Montrail-Womens-Sneaker-Regular/dp/B07JL4NSS6/ref=sr_1_3?crid=1J645HNW4APKF&keywords=columbia+montrail+bajada&qid=1659015757&sprefix=columbia+montrail+b%2Caps%2C294&sr=8-3",
-          "_blank"
+          'https://www.amazon.com/Columbia-Montrail-Womens-Sneaker-Regular/dp/B07JL4NSS6/ref=sr_1_3?crid=1J645HNW4APKF&keywords=columbia+montrail+bajada&qid=1659015757&sprefix=columbia+montrail+b%2Caps%2C294&sr=8-3',
+          '_blank'
         );
         break;
 
-      case "gucci":
+      case 'gucci':
         window.open(
-          "https://www.gucci.com/us/en/pr/women/shoes-for-women/sneakers-for-women/womens-ace-embroidered-platform-sneaker-p-577573DOPE09064",
-          "_blank"
+          'https://www.gucci.com/us/en/pr/women/shoes-for-women/sneakers-for-women/womens-ace-embroidered-platform-sneaker-p-577573DOPE09064',
+          '_blank'
         );
         break;
 
-      case "newBalance":
+      case 'newBalance':
         window.open(
-          "https://www.amazon.com/New-Balance-Fresh-Running-Aluminum/dp/B093QJH239/ref=sr_1_7?keywords=new%2Bbalance%2Bshoes&qid=1659009217&sr=8-7&th=1",
-          "_blank"
+          'https://www.amazon.com/New-Balance-Fresh-Running-Aluminum/dp/B093QJH239/ref=sr_1_7?keywords=new%2Bbalance%2Bshoes&qid=1659009217&sr=8-7&th=1',
+          '_blank'
         );
         break;
 
-      case "nikeZoom":
+      case 'nikeZoom':
         window.open(
-          "https://www.amazon.in/NIKE-Pegasus-Running-Sport-Shoes/dp/B07C1PJM3B",
-          "_blank"
+          'https://www.amazon.in/NIKE-Pegasus-Running-Sport-Shoes/dp/B07C1PJM3B',
+          '_blank'
         );
         break;
 
-      case "sieviRacer":
+      case 'sieviRacer':
         window.open(
-          "https://www.sievi.com/products/safety-shoes/sievi-racer-roller-s3",
-          "_blank"
+          'https://www.sievi.com/products/safety-shoes/sievi-racer-roller-s3',
+          '_blank'
         );
         break;
 
-      case "vans":
+      case 'vans':
         window.open(
-          "https://www.amazon.com/Vans-Unisex-Sk8-Hi-Black-White/dp/B001C0LIVC/ref=sr_1_37?keywords=Vans+Shoes&qid=1659008629&sr=8-37",
-          "_blank"
+          'https://www.amazon.com/Vans-Unisex-Sk8-Hi-Black-White/dp/B001C0LIVC/ref=sr_1_37?keywords=Vans+Shoes&qid=1659008629&sr=8-37',
+          '_blank'
         );
         break;
 
@@ -672,4 +672,4 @@ window.addEventListener("pointerup", () => {
   isDragging = false;
   isMouseDOwn = false;
 });
-window.addEventListener("click", () => {});
+window.addEventListener('click', () => {});
